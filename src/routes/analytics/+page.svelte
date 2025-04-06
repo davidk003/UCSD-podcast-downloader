f62cebc27223e886c987c6e9f07d59cde500d533
david <dck003@ucsd.edu>
2025-04-05 22:52:54 -0700

    Needs charts but counters finished for analytics page

--- src/routes/analytics/+page.svelte
+++ src/routes/analytics/+page.svelte
@@ -1,18 +1,47 @@
 <script lang="ts">
-    import Button from '$lib/components/ui/button/button.svelte';
-import NumberFlow, { continuous } from '@number-flow/svelte';
-    const numbers = [0, 100, 200, 300, 400, 500];
-    let idx = 0;
-    $: value = numbers[idx % numbers.length];
+	import Button from '$lib/components/ui/button/button.svelte';
+    import { SvelteMap } from 'svelte/reactivity';
+	import NumberFlow, { continuous } from '@number-flow/svelte';
+	
 
-    function incrementIndex() {
-        idx++;
+    let counters = new SvelteMap<string, number>([
+        ["Videos scraped", 0],
+        ["Videos served", 0],
+        ["Average response time", 0],
+        ["Unique Classes", 0]
+    ]);
+	$: value = Math.floor(Math.random() * Math.random() * 10000000);
+
+    setInterval(() => {
+        updateCounts();
+    }, 1000);
+
+    function updateCounts() {
+
+        for (const [key, value] of counters) {
+            counters.set(key, Math.floor(Math.random() * Math.random() * 10000000));
+        }
+
     }
 </script>
 
-<h1 class="text-3xl font-semibold text-center m-5">Analytics</h1>
-<div>
-    <NumberFlow value={value} trend={0} format={{ notation: "compact" }} class=""/>
-    <Button on:click={incrementIndex} class="m-5">
-    </Button>
+<h1 class="m-5 text-center text-3xl font-semibold">Analytics</h1>
+
+<div class="flex flex-col items-center justify-center gap-4 md:flex-row">
+
+{#each counters as [key, value]}
+<div class="flex flex-col items-center">
+    <h3 class="m-5 text-center text-2xl font-semibold">{key}</h3>
+    <NumberFlow
+        class="gap-4 font-semibold text-4xl"
+        {value}
+        locales="en-US"
+        trend={+1}
+        plugins={[continuous]}
+        transformTiming={{ duration: 750, easing: 'linear(...)' }}
+        spinTiming={{ duration: 750, easing: 'linear(...)' }}
+        opacityTiming={{ duration: 350, easing: 'ease-out' }}
+    />
+</div>
+{/each}
 </div>
